81efc1e90dc43e1d3214fa8e1fc1b4a38383a4d2
ErElectrical <94623283+ErElectrical@users.noreply.github.com>
2022-01-05 11:17:36 +0000

    Unit Test for Program

---
+++ UnitTest1.cs
@@ -0,0 +1,88 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzer;
+
+namespace MoodAnalyzerUnittest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //create a instance for algorithum class
+        private readonly MoodAnalyzer.MoodAnalyzer Program;
+
+        private readonly MoodAnalyzer.RefactorConstructor refactor;
+
+        // constructor of class our class
+
+        public UnitTest1()
+        {
+            Program = new MoodAnalyzer.MoodAnalyzer();
+            refactor = new MoodAnalyzer.RefactorConstructor();
+        }
+
+        [TestMethod]
+
+        // Test case 1
+        public void TestMethod1()
+        {
+            //Arrange
+            string input = "I am in sad mood";
+            //Act
+            var result = Program.AnalyzeMood(input);
+
+            //Assert
+
+            Assert.AreEqual("sad Mood", result);
+
+        }
+
+
+        //Test case 2
+        public void TestMethod2()
+        {
+            //Arrange
+            string input = "I am in any mood";
+            //Act
+            var result = Program.AnalyzeMood(input);
+
+            //Assert
+
+            Assert.AreEqual("Happy Mood", result);
+
+
+        }
+
+        //Test case1 for Refactor constructor
+        public void TestMethod3_RefactorConstructor()
+        {
+            // Arrange 
+            string input = " I am in sad Mood";
+
+            RefactorConstructor refector = new RefactorConstructor(input);
+
+            //Act
+
+            var result = refactor.AnalyzeMood();
+
+            //Assert
+            Assert.AreEqual("sad mood", result);
+
+        }
+
+        //Test case2 for refactor constructor
+        public void TestMethod4_RefactorConstructor()
+        {
+            // Arrange 
+            string input = " I am in happy Mood";
+
+            RefactorConstructor refector = new RefactorConstructor(input);
+
+            //Act
+
+            var result = refactor.AnalyzeMood();
+
+            //Assert
+            Assert.AreEqual("happy mood", result);
+
+        }
+    }
+}
